3f15f7b4460ce9ae24e9ff23eef5462c6cdf4dde
Armano den Boef <t.a.denboef@gmail.com>
2022-01-03 17:06:00 +0100

    Remove excess accessibility

--- Taurus/Commands/Parsers/EntityParsers.cs
+++ Taurus/Commands/Parsers/EntityParsers.cs
@@ -8,17 +8,17 @@ namespace Taurus.Commands.Parsers
     {
         public static UserAccount ParseUserAccount(string s)
         {
-
+            throw new NotImplementedException();
         }
 
         public static UserGroup ParseUserGroup(string s)
         {
-
+            throw new NotImplementedException();
         }
 
         public static Region ParseRegion(string s)
         {
-
+            throw new NotImplementedException();
         }
     }
 }
--- Taurus/Commands/Parsers/Parser.cs
+++ Taurus/Commands/Parsers/Parser.cs
@@ -9,49 +9,44 @@ namespace Taurus.Commands.Parsers
 {
     public static class Parser
     {
-        private static readonly Lazy<Dictionary<Type, Func<string, object>>> _baseParsers = new(GeneratePrimitive);
-
-        private static Dictionary<Type, Func<string, object>> GeneratePrimitive()
+        private static readonly Dictionary<Type, Func<string, object>> _baseParsers = new()
         {
-            var func = new Dictionary<Type, Func<string, object>>();
-            func[typeof(byte)] = (s) => byte.Parse(s);
-            func[typeof(sbyte)] = (s) => sbyte.Parse(s);
-            func[typeof(short)] = (s) => short.Parse(s);
-            func[typeof(ushort)] = (s) => ushort.Parse(s);
-            func[typeof(int)] = (s) => int.Parse(s);
-            func[typeof(uint)] = (s) => uint.Parse(s);
-            func[typeof(nint)] = (s) => nint.Parse(s);
-            func[typeof(nuint)] = (s) => nuint.Parse(s);
-            func[typeof(long)] = (s) => long.Parse(s);
-            func[typeof(ulong)] = (s) => ulong.Parse(s);
-            func[typeof(float)] = (s) => float.Parse(s);
-            func[typeof(double)] = (s) => double.Parse(s);
-            func[typeof(decimal)] = (s) => decimal.Parse(s);
-            func[typeof(bool)] = (s) => bool.Parse(s);
-
-            func[typeof(UserAccounts.UserAccount)] = (s) => EntityParsers.ParseUserAccount(s);
-            func[typeof(UserGroups.UserGroup)] = (s) => EntityParsers.ParseUserGroup(s);
-            func[typeof(Regions.Region)] = (s) => EntityParsers.ParseRegion(s);
-            func[typeof(TimeSpan)] = (s) => TimeParser.ParseTimeSpan(s);
-            func[typeof(DateTime)] = (s) => TimeParser.ParseDateTime(s);
-            return func;
-        }
+            { typeof(byte), (s) => byte.Parse(s) },
+            { typeof(sbyte), (s) => sbyte.Parse(s) },
+            { typeof(short), (s) => short.Parse(s) },
+            { typeof(ushort), (s) => ushort.Parse(s) },
+            { typeof(int), (s) => int.Parse(s) },
+            { typeof(uint), (s) => uint.Parse(s) },
+            { typeof(nint), (s) => nint.Parse(s) },
+            { typeof(nuint), (s) => nuint.Parse(s) },
+            { typeof(long), (s) => long.Parse(s) },
+            { typeof(ulong), (s) => ulong.Parse(s) },
+            { typeof(float), (s) => float.Parse(s) },
+            { typeof(double), (s) => double.Parse(s) },
+            { typeof(decimal), (s) => decimal.Parse(s) },
+            { typeof(bool), (s) => bool.Parse(s) },
+            { typeof(UserAccounts.UserAccount), (s) => EntityParsers.ParseUserAccount(s) },
+            { typeof(UserGroups.UserGroup), (s) => EntityParsers.ParseUserGroup(s) },
+            { typeof(Regions.Region), (s) => EntityParsers.ParseRegion(s) },
+            { typeof(TimeSpan), (s) => TimeParser.ParseTimeSpan(s) },
+            { typeof(DateTime), (s) => TimeParser.ParseDateTime(s) }
+        };
 
         public static object GetValue(Type t, string parameter)
-            => _baseParsers.Value[t]?.Invoke(parameter) ?? throw new ArgumentNullException(nameof(t), "No parser found for this type!");
+            => _baseParsers[t]?.Invoke(parameter) ?? throw new ArgumentNullException(nameof(t), "No parser found for this type!");
 
         public static Func<string, object> GetDelegate(Type t)
-            => _baseParsers.Value[t] ?? throw new ArgumentNullException(nameof(t), "No parser found for this type.");
+            => _baseParsers[t] ?? throw new ArgumentNullException(nameof(t), "No parser found for this type.");
 
         public static bool Add(Type t, Func<string, object> parser)
         {
-            if (_baseParsers.Value.ContainsKey(t))
+            if (_baseParsers.ContainsKey(t))
                 return false;
-            _baseParsers.Value.Add(t, parser);
+            _baseParsers.Add(t, parser);
             return true;
         }
 
         public static bool Remove(Type t)
-            => _baseParsers.Value.Remove(t);
+            => _baseParsers.Remove(t);
     }
 }
