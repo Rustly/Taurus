160e474456dc66773e98cf96f07ff6af69931dc5
Armano den Boef <t.a.denboef@gmail.com>
2022-01-03 17:27:13 +0100

    Some consistency in parsers

--- Taurus/Commands/Parsers/Parser.cs
+++ Taurus/Commands/Parsers/Parser.cs
@@ -9,28 +9,31 @@ namespace Taurus.Commands.Parsers
 {
     public static class Parser
     {
-        private static readonly Dictionary<Type, Func<string, object>> _baseParsers = new()
-        {
-            { typeof(byte), (s) => byte.Parse(s) },
-            { typeof(sbyte), (s) => sbyte.Parse(s) },
-            { typeof(short), (s) => short.Parse(s) },
-            { typeof(ushort), (s) => ushort.Parse(s) },
-            { typeof(int), (s) => int.Parse(s) },
-            { typeof(uint), (s) => uint.Parse(s) },
-            { typeof(nint), (s) => nint.Parse(s) },
-            { typeof(nuint), (s) => nuint.Parse(s) },
-            { typeof(long), (s) => long.Parse(s) },
-            { typeof(ulong), (s) => ulong.Parse(s) },
-            { typeof(float), (s) => float.Parse(s) },
-            { typeof(double), (s) => double.Parse(s) },
-            { typeof(decimal), (s) => decimal.Parse(s) },
-            { typeof(bool), (s) => bool.Parse(s) },
-            { typeof(UserAccounts.UserAccount), (s) => EntityParsers.ParseUserAccount(s) },
-            { typeof(UserGroups.UserGroup), (s) => EntityParsers.ParseUserGroup(s) },
-            { typeof(Regions.Region), (s) => EntityParsers.ParseRegion(s) },
-            { typeof(TimeSpan), (s) => TimeParser.ParseTimeSpan(s) },
-            { typeof(DateTime), (s) => TimeParser.ParseDateTime(s) }
-        };
+        private static readonly Dictionary<Type, Func<string, object>> _baseParsers;
+
+        static Parser()
+            => _baseParsers = new()
+            {
+                { typeof(byte), (s) => byte.Parse(s) },
+                { typeof(sbyte), (s) => sbyte.Parse(s) },
+                { typeof(short), (s) => short.Parse(s) },
+                { typeof(ushort), (s) => ushort.Parse(s) },
+                { typeof(int), (s) => int.Parse(s) },
+                { typeof(uint), (s) => uint.Parse(s) },
+                { typeof(nint), (s) => nint.Parse(s) },
+                { typeof(nuint), (s) => nuint.Parse(s) },
+                { typeof(long), (s) => long.Parse(s) },
+                { typeof(ulong), (s) => ulong.Parse(s) },
+                { typeof(float), (s) => float.Parse(s) },
+                { typeof(double), (s) => double.Parse(s) },
+                { typeof(decimal), (s) => decimal.Parse(s) },
+                { typeof(bool), (s) => bool.Parse(s) },
+                { typeof(UserAccounts.UserAccount), (s) => EntityParsers.ParseUserAccount(s) },
+                { typeof(UserGroups.UserGroup), (s) => EntityParsers.ParseUserGroup(s) },
+                { typeof(Regions.Region), (s) => EntityParsers.ParseRegion(s) },
+                { typeof(TimeSpan), (s) => TimeParser.ParseTimeSpan(s) },
+                { typeof(DateTime), (s) => TimeParser.ParseDateTime(s) }
+            };
 
         public static object GetValue(Type t, string parameter)
             => _baseParsers[t]?.Invoke(parameter) ?? throw new ArgumentNullException(nameof(t), "No parser found for this type!");
--- Taurus/Commands/Parsers/TimeParser.cs
+++ Taurus/Commands/Parsers/TimeParser.cs
@@ -2,20 +2,85 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Taurus.Commands.Parsers
 {
     internal static class TimeParser
     {
+        private delegate TimeSpan CallBackDelegate(string match);
+
+        private static readonly Dictionary<string, CallBackDelegate> _callBackResult;
+
+        private static readonly Regex _regex = new(@"(\d*)\s*([a-zA-Z]*)\s*(?:and|,)?\s*");
+
+        static TimeParser() 
+            => _callBackResult = new()
+            {
+                { "second", Seconds },
+                { "seconds", Seconds },
+                { "sec", Seconds },
+                { "s", Seconds },
+                { "minute", Minutes },
+                { "minutes", Minutes },
+                { "min", Minutes },
+                { "m", Minutes },
+                { "hour", Hours },
+                { "hours", Hours },
+                { "h", Hours },
+                { "day", Days },
+                { "days", Days },
+                { "d", Days },
+                { "week", Weeks },
+                { "weeks", Weeks },
+                { "w", Weeks },
+                { "month", Months },
+                { "months", Months },
+            };
+
+        private static TimeSpan Seconds(string match)
+            => new(0, 0, int.Parse(match));
+
+        private static TimeSpan Minutes(string match)
+            => new(0, int.Parse(match), 0);
+
+        private static TimeSpan Hours(string match)
+            => new(int.Parse(match), 0, 0);
+
+        private static TimeSpan Days(string match)
+            => new(int.Parse(match), 0, 0, 0);
+
+        private static TimeSpan Weeks(string match)
+            => new((int.Parse(match) * 7), 0, 0, 0);
+
+        private static TimeSpan Months(string match)
+            => new((int.Parse(match) * 30), 0, 0, 0);
+
         public static DateTime ParseDateTime(string s)
         {
-            throw new NotImplementedException();
+            if (!DateTime.TryParse(s, out DateTime time))
+            {
+                time = DateTime.Now;
+            }
+            return time;
         }
 
         public static TimeSpan ParseTimeSpan(string s)
         {
-            throw new NotImplementedException();
+            if (!TimeSpan.TryParse(s, out TimeSpan span))
+            {
+                s = s.ToLower().Trim();
+                MatchCollection matches = _regex.Matches(s);
+                if (matches.Any())
+                    foreach (Match match in matches)
+                    {
+                        if (_callBackResult.TryGetValue(match.Groups[2].Value, out var callback))
+                            span += callback(match.Groups[1].Value);
+                        else continue;
+                    }
+            }
+            return span;
         }
     }
 }
